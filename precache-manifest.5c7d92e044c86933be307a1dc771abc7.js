5359d33eb48fc18ee65d4b84fd6c9b2b3e5f7b77
Alexey Alexandrov <a.alexandrov@aerolabs.ru>
2020-09-11 14:45:56 +0300

    Updates

---
+++ precache-manifest.5c7d92e044c86933be307a1dc771abc7.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7de088624f24772c4fc05c16020819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce6acf59020960e8492",
+    "url": "/static/css/main.f6cd43db.chunk.css"
+  },
+  {
+    "revision": "1f5b0807fb66c03fd5a2",
+    "url": "/static/js/2.17c9d130.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17c9d130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce6acf59020960e8492",
+    "url": "/static/js/main.9a52f0d1.chunk.js"
+  },
+  {
+    "revision": "405f69952b91b14fb415",
+    "url": "/static/js/runtime-main.e60c01e6.js"
+  },
+  {
+    "revision": "5213488fa3c3fe276d6810e138664494",
+    "url": "/static/media/1000ml.5213488f.png"
+  },
+  {
+    "revision": "23981b91926876057ab79d4d77a66a6e",
+    "url": "/static/media/100ml.23981b91.png"
+  },
+  {
+    "revision": "5a2c888d6a791f4ca30cd72a88ddd853",
+    "url": "/static/media/250ml.5a2c888d.png"
+  },
+  {
+    "revision": "636ff6b21621f8b888a4ee4afb1679bb",
+    "url": "/static/media/500ml.636ff6b2.png"
+  },
+  {
+    "revision": "bd0c595400f8e0e6d6a0c383fca35003",
+    "url": "/static/media/Beauty-Face.bd0c5954.svg"
+  },
+  {
+    "revision": "210b7908b89f172cd67bc090406947f1",
+    "url": "/static/media/Beauty-Hand-Cream.210b7908.svg"
+  },
+  {
+    "revision": "1ff4ebb798e62c92116c9fcb0fbb91a6",
+    "url": "/static/media/Beauty-Moisturizer-Cream.1ff4ebb7.svg"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "bdc1fd7c2a35112242865c525d7c7975",
+    "url": "/static/media/PlayfairDisplay-Black.bdc1fd7c.ttf"
+  },
+  {
+    "revision": "d27b6b12d96d9cf68f493c78113ce390",
+    "url": "/static/media/PlayfairDisplay-Bold.d27b6b12.ttf"
+  },
+  {
+    "revision": "68ac1d23ba6429b7dea082e8e761cde3",
+    "url": "/static/media/PlayfairDisplay-SemiBold.68ac1d23.ttf"
+  },
+  {
+    "revision": "ab5fb31d6ab648826a48c59f6c49651f",
+    "url": "/static/media/VK.ab5fb31d.svg"
+  },
+  {
+    "revision": "fde2b1ae770ad69250c8a4589568086c",
+    "url": "/static/media/adv_bg.fde2b1ae.svg"
+  },
+  {
+    "revision": "44ea9a3d11423b8029f217359d0ef722",
+    "url": "/static/media/advantage.44ea9a3d.svg"
+  },
+  {
+    "revision": "02adb32de4356b604d5fd752bc3bab4c",
+    "url": "/static/media/arrov.02adb32d.svg"
+  },
+  {
+    "revision": "2325b0af13833e3c6c398444ef588835",
+    "url": "/static/media/arrow2.2325b0af.svg"
+  },
+  {
+    "revision": "20c3d3087c71dd3be5dcb4d43b86bbb3",
+    "url": "/static/media/bottle.20c3d308.png"
+  },
+  {
+    "revision": "1a3e461a9f49c00e3c4df887e567c93c",
+    "url": "/static/media/bottle_480.1a3e461a.png"
+  },
+  {
+    "revision": "c2b084aeee5a26331f74791b4e552275",
+    "url": "/static/media/energy.c2b084ae.jpg"
+  },
+  {
+    "revision": "297c8a0a962c0f1b2d312380a9145df8",
+    "url": "/static/media/energy_bg.297c8a0a.svg"
+  },
+  {
+    "revision": "3072331d249756181e934c2458530171",
+    "url": "/static/media/energy_icon.3072331d.svg"
+  },
+  {
+    "revision": "046fa00d5bc1f29face4b61774575586",
+    "url": "/static/media/foto.046fa00d.png"
+  },
+  {
+    "revision": "d5421417627736c741e109c512dcf782",
+    "url": "/static/media/glass.d5421417.svg"
+  },
+  {
+    "revision": "8f92a39af482f1ab4f78b62f1e21ee30",
+    "url": "/static/media/insta.8f92a39a.svg"
+  },
+  {
+    "revision": "3243d8eb5e5396571bae5c1d2024c184",
+    "url": "/static/media/lead_bg.3243d8eb.png"
+  },
+  {
+    "revision": "90a3c5f243bf13f15daa22af8aa3439b",
+    "url": "/static/media/lead_bg_1024.90a3c5f2.png"
+  },
+  {
+    "revision": "5e3d1f9fae9c09c40dfb702945432c9c",
+    "url": "/static/media/lead_bg_1920.5e3d1f9f.png"
+  },
+  {
+    "revision": "54a51a75b616d52e600b29dd55484659",
+    "url": "/static/media/lead_bg_480.54a51a75.png"
+  },
+  {
+    "revision": "f66e3acb9f2fb543355b7937532372b2",
+    "url": "/static/media/lead_image.f66e3acb.svg"
+  },
+  {
+    "revision": "6e876e664d45972a38a10a9a56757b9e",
+    "url": "/static/media/main.6e876e66.png"
+  },
+  {
+    "revision": "67e4969e6e1b24e4657eac24e99d6466",
+    "url": "/static/media/main_480.67e4969e.svg"
+  },
+  {
+    "revision": "daac827017a5c2712ee5aab8666aadf8",
+    "url": "/static/media/massage.daac8270.svg"
+  },
+  {
+    "revision": "a81420d9061edcc9caf575a002a66418",
+    "url": "/static/media/options_bg.a81420d9.png"
+  },
+  {
+    "revision": "7c1e549e1d29f5edff5ded4ae5b8c9ce",
+    "url": "/static/media/order.7c1e549e.svg"
+  },
+  {
+    "revision": "0846a673d846c00974a840b207deb945",
+    "url": "/static/media/order_icon.0846a673.svg"
+  },
+  {
+    "revision": "2363b5da7889f9a6f43bad617356be95",
+    "url": "/static/media/otzovik.2363b5da.svg"
+  },
+  {
+    "revision": "5e264d3959200cdd6279a12e2c0bf67b",
+    "url": "/static/media/phone.5e264d39.svg"
+  },
+  {
+    "revision": "6f93f7ff53636d9b01a62c8921a81e95",
+    "url": "/static/media/salat.6f93f7ff.svg"
+  }
+]);
